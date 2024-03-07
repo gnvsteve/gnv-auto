fbdff391e5797731cdc45e4600f7276054126f41
gnvsteve <157470127+gnvsteve@users.noreply.github.com>
2024-03-07 20:10:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+
+
+
+function changeStyle() {
+    var span = document.getElementById("hilux2018");
+    if(span.style.display === "block") {
+        span.style.display = "none";
+    } else {
+        span.style.display = "block";
+    }
+    }
+
+
+
+function changeStyle1() {
+    var span = document.getElementById("range-rover");
+    if(span.style.display === "block") {
+        span.style.display = "none";
+    } else {
+        span.style.display = "block";
+    }
+    }
+
+    function changeStyle2() {
+        var span = document.getElementById("hilux2020");
+        if(span.style.display === "block") {
+            span.style.display = "none";
+        } else {
+            span.style.display = "block";
+        }
+        }
+
+        function changeStyle4() {
+            var span = document.getElementById("bmw-jeep");
+            if(span.style.display === "block") {
+                span.style.display = "none";
+            } else {
+                span.style.display = "block";
+            }
+            }
+
+            function changeStyle3() {
+                var span = document.getElementById("lamborghini");
+                if(span.style.display === "block") {
+                    span.style.display = "none";
+                } else {
+                    span.style.display = "block";
+                }
+                }
+
+                function changeStyle5() {
+                    var span = document.getElementById("bmw-salon");
+                    if(span.style.display === "block") {
+                        span.style.display = "none";
+                    } else {
+                        span.style.display = "block";
+                    }
+                    }
+
+
+
+                    function changeStyle7() {
+                        var span = document.getElementById("click here for gnv-auto-no");
+                        if(span.style.display === "block") {
+                            span.style.display = "none";
+                        } else {
+                            span.style.display = "block";
+                        }
+                        }
